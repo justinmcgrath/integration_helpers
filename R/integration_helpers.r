52e4ac5a21fc0592c65131a31155f258158e5cd2
Justin McGrath <justinmcgrath@users.noreply.github.com>
2018-05-17 10:39:51 -0500

    Add chi-square calculated and update documentation.

--- R/integration_helpers.r
+++ R/integration_helpers.r
@@ -1,12 +1,12 @@
 # When integrating, the state at an arbitraty time must be known, but typically data are available at discontinuous times.
 # R provides functions to map a discontinuous function to a continuous one, e.g., approxfun(), but does not provide similar functions for
-# data frames. This provides an interpolator, which given a discrete function of x and an interpolation, returns a continuous function of x.
+# data frames. This function provides an interpolater that, given a discrete function of x and an interpolation method, returns a continuous function of x.
 
 # Given a data frame, and an interpolation method, return an interpolation function.
 # continuous_df = interpolater(df, x_name, method)
 # df: the data to interpolate (a data frame).
-# x_name: the name of the column that df that will be the input of the continous function (a character vector).
-# method: a function of the form f(x, y), that returns g(x) where g is a continuous function x (a function).
+# x_name: the name of the column in df that will be the input of the continuous function (a character vector).
+# method: a function of the form f(x, y), that returns g(x) where g is a continuous function of x (a function).
 
 # Example:
 # df = data.frame(x = c(1, 2, 3), y1 = c(2, 5, 8), y2=c(3, 8, 5))
@@ -41,6 +41,37 @@ interpolater = function(df, x_name, method) {
     return(interpolate)
 }
 
+
+# It's common to minimize sum of squares when optimizing.
+# If using multiple dependent variables, the sums of squares should be normalized by the predicted value, which is chi-squared distributed.
+# This function returns the sum of the chi values for the difference between observed and predicted values.
+# observed_df: the observed data - a data frame.
+# x_name: the name of the independent variable.
+# predicted_df: the predicted data - data frame.
+# The sum of squares is calculated for columns with the same names in observed_df and predicted_df, except for the x_name column.
+
+chi_difference = function(observed_df, x_name, predicted_df) {
+        y_names = setdiff(intersect(names(observed_df), names(predicted_df)), x_name)
+        i_result = interpolater(predicted_df[c(y_names, x_name)], x_name, approxfun)
+
+        ss = 0
+        for (y in y_names) {
+            pred_y = i_result(observed_df[[x_name]])[[y]]  # Predict y at all observed times.
+
+            obs_y = observed_df[[y]] 
+            undefined_ind = pred_y == 0 & obs_y == 0
+            ss_y = abs(sum(((obs_y - pred_y)^2 / pred_y)[!undefined_ind], na.rm=TRUE))  # Chi squared should always be positive, but it's possible for pred_y to be negative.
+            ss = ss + ss_y
+        }
+        return (ss)
+}
+
+chi_difference_fun = function(obsered_df, x_name, predict_func) {
+    function(...) {
+        chi_difference(obsered_df, x_name, predict_func(...))
+    }
+}
+
 # During integration, if the integrater does not provide a way to report progress then it is helpful to have the function being integrater report progress as it is called.
 # Here a class is defined that will be called during each step of the integration and can be used to report progress.
 # reporter
@@ -65,9 +96,11 @@ interpolater = function(df, x_name, method) {
 
 # time_reporter
 # Shows a progress bar that reporters the number of times the function has been called and the current time step.
-# In addition to update() and reset() it also provides
+#
 # counter() - Returns the number times the reporter was updated (an integer).
 # iter_times() - Returns the time step at each call of the reporter (a numeric vector, where the index of the vector is the interation step).
+# update() - see reporter definition.
+# reset() - see reporter definition.
 
 time_reporter = function(ode_func=NULL) {
     stopifnot(is.null(ode_func) || is.function(ode_func))
@@ -112,18 +145,21 @@ time_reporter = function(ode_func=NULL) {
           } else {
               NULL
           }
-    return(list(ode=ode, counter=counter, iter_times=iter_times, update=update, reset=reset))
+    return (list(ode=ode, counter=counter, iter_times=iter_times, update=update, reset=reset))
 }
 
 # state_reporter
 # Records the state during the integration.
 # Some variable time step integraters use appropriate time steps to ensure the function is linear for any given time step, but then do not report the state at those times: e.g., many solvers in the deSolve package.
-# That is bizarre behavior. This attempts to determine appropriate times to store the state.
+# That is bizarre behavior.
+# This class attempts to determine appropriate times to store the state.
 # The reporter can include unused integration steps, so it can be used to determine what the integrater is doing, but the data
 # from it shouldn't be relied on. If you're using a bizarre solver, and you need to data at certain times,
 # you could use this to determine appropriate times, and solve the system again using those times as input.
-# In addition to update() and reset() it also provides
+#
 # state() - Returns a data frame of the state at every time step that is potentially important to get linearity between times. WARNING: It may contain integration steps that were not acctually used in solving the system.
+# update() - see reporter definition.
+# reset() - see reporter definition.
 
 state_reporter = function(ode_func=NULL) {
     stopifnot(is.null(ode_func) || is.function(ode_func))
@@ -169,7 +205,7 @@ state_reporter = function(ode_func=NULL) {
         }
         state_list[[state_index]] <<- unlist(state)
         times[state_index] <<- time
-        invisible()
+        invisible ()
     }
 
     state = function() {
@@ -192,7 +228,7 @@ state_reporter = function(ode_func=NULL) {
               NULL
           }
 
-    return(list(ode=ode, reset=reset, update=update, state=state))
+    return (list(ode=ode, reset=reset, update=update, state=state))
 }
 
 # combine_reporters
